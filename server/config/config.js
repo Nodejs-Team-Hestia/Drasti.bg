6d3852714398b3e571634d250c1fe70c9bca26fd
Martin Anastasov <m4rt00@gmail.com>
2014-10-08 18:18:50 +0300

    Added some props in User schema

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
 var path = require('path');
-var ROOT_PATH = path.normalize(__dirname + '../../');
-var PORT = 5000;
+
+var ROOT_PATH = path.normalize(__dirname + '../../'),
+    PORT = 5000,
+    DB_NAME = 'drastiDb';
 
 module.exports = {
     development: {
         rootPath: ROOT_PATH,
-        db: 'mongodb://localhost/drastiDb',
+        db: 'mongodb://localhost/' + DB_NAME,
         port: process.env.PORT || PORT
     },
     production: {
---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    firstName: {type: String},
+    lastName: {type: String},
+    dateOfBirth: Date,
+    location: {
+        city: String,
+        country: String
+    },
+    albums: [
+        {type: Schema.ObjectId, ref: 'Album'}
+    ],
+    friends: [
+        {type: Schema.ObjectId, ref: 'User'}
+    ],
+    salt: String,
+    hashPassword: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
